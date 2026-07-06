5f2829cde04ac6bd67344036b5e1b38c6406d5e9
Juan José Chiroque <juanjosechiroque@gmail.com>
2026-07-06 11:07:52 -0500

    feat: add health endpoint

---
+++ src/api/health/health.router.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { healthCheckHandler } from "./health.controller.js";
+
+const router = Router();
+
+router.get("/", healthCheckHandler);
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
+import healthRouter from "../api/health/health.router.js";
 import stripeRoutes from "../stripe/stripe.routes.js";
 
 const router = Router();
 
-router.get("/", (_req, res) => {
-    res.json({ status: "running" });
-});
-
+router.use("/health", healthRouter);
 router.use("/stripe", stripeRoutes);
 
 export default router;
